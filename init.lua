c6cdfa876530f8f74145c42e9aa6b2892d512af9
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-02-23 12:51:24 -0600

    Comment out theme italic in comments thingy.

--- init.lua
+++ init.lua
@@ -918,12 +918,12 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+      -- ---@diagnostic disable-next-line: missing-fields
+      -- require('onedark').setup {
+      --   styles = {
+      --     comments = { italic = false }, -- Disable italics in comments
+      --   },
+      -- }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
