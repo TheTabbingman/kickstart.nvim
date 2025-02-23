7dc707b04de54cf7a2bfaf93778e937dd4269716
TheTabbingMan <51281790+TheTabbingman@users.noreply.github.com>
2025-02-23 12:38:39 -0600

    Remove weird comment line

--- init.lua
+++ init.lua
@@ -653,7 +653,6 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         eslint = {},
-        --
 
         lua_ls = {
           -- cmd = { ... },
